27b883f7a89ee835245edcf4cbb185b8094224ac
Ambati Niranjana Srinivasu <ambati.ns04@gmail.com>
2023-11-07 17:01:29 +0530

    Project code submission

---
+++ Project Code/app/src/main/java/com/example/chatconnect/FCMNotificationService.java
@@ -0,0 +1,6 @@
+package com.example.chatconnect;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FCMNotificationService extends FirebaseMessagingService {
+}
---
+++ Project Code/settings.gradle.kts
@@ -0,0 +1,22 @@
+// For plugin management
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(uri("https://jitpack.io"))
+    }
+}
+
+rootProject.name = "ChatConnect"
+include(":app")
+
+ 
